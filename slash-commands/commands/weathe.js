330ee4f70c9bf8b7eb3ad80ec6d53fa0dea8c83b
jamal edine benrahmoun <benrahmounjamaleddine@gmail.com>
2022-12-24 23:02:19 +0100

    activating the /weather api

--- slash-commands/commands/weathe.js
+++ slash-commands/commands/weathe.js
@@ -1,5 +1,7 @@
 import { SlashCommandBuilder } from 'discord.js';
-
+import fetch from 'node-fetch';
+import dotenv from "dotenv" ;
+dotenv.config();
  const data = new SlashCommandBuilder()
     .setName('weather')
     .setDescription('give you the wither in')
@@ -7,9 +9,36 @@ import { SlashCommandBuilder } from 'discord.js';
         .setDescription('the city you wanna know the wather') 
     );
 async function execute(interaction) {
-    const city = interaction.options.getString('city'); // ?? if city not added use the localisation from navigator cookies
-    console.log(city);
+   // ?? if city not added use the localisation from navigator cookies
+const city = await interaction.options.getString('city');  
+   //api key from .env
+const key = process.env.keys
+
+const getWeather= async(id)=>{
+    const base = `http://dataservice.accuweather.com/currentconditions/v1/`;
+const query = `${id}?apikey=${key}`;
+    const response = await fetch(base+query);
+const data = await response.json();
+return data[0]
+};
+
+const getCity = async (city)=>{
+const base = `http://dataservice.accuweather.com/locations/v1/cities/search`;
+const query = `?apikey=${key}&q=${city}`;
+    const responsed = await fetch(base+query);
+const data = await responsed.json();
+return data[0]
+};
+
+
+//get the city 
+
+getCity(city).then(data=>{
+    return getWeather(data.Key)}).then(data=>{
+        interaction.reply( `in the city off ${city} the weather is ${data.WeatherText} the temperature is ${data.Temperature.Metric.Value} C°  `)
+    })
 }
+
          
-export {data,execute}
+export { data , execute }
     
