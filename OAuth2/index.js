79dd56c14fdbaafdfc9dc3592be1777047482565
jamal edine benrahmoun <benrahmounjamaleddine@gmail.com>
2023-01-02 16:19:01 +0100

    some express featurs

---
+++ OAuth2/index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import { request } from 'undici';
+import config from './config.json' assert {type : "json"};
+const port = 53134;
+const app = express();
+
+app.get('/', async ({ query }, response) => {
+	const { code } = query;
+
+	if (code) {
+		try {
+			const tokenResponseData = await request('https://discord.com/api/oauth2/token', {
+				method: 'POST',
+				body: new URLSearchParams({
+					client_id: config.clientId,
+					client_secret: config.clientSecret,
+					code,
+					grant_type: 'authorization_code',
+					redirect_uri: `http://localhost:${port}`,
+					scope: 'identify',
+				}).toString(),
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+			});
+
+			const oauthData = await tokenResponseData.body.json();
+			console.log(oauthData);
+		} catch (error) {
+			// NOTE: An unauthorized token will not throw an error
+			// tokenResponseData.statusCode will be 401
+			console.error(error);
+		}
+	}
+
+	return response.sendFile('index.html', { root: '/home/jamal/web/discord/bots/OAuth2/' });
+})
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+
+export default app.get 
